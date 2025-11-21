4f2d5f1b5ca45381c029333e7e3c35793f25b2e5
dutudev <dutudev@gmail.com>
2025-11-21 19:17:09 +0200

    made paddles

--- Main.cpp
+++ Main.cpp
@@ -1,15 +1,52 @@
 #include "raylib.h"
+#include <iostream>
 
 using namespace std;
+
+const Vector2 paddleSize = { 10, 100 };
+
+//make classes here, maybe move them to a separate folder later
+class Paddle {
+	private:
+		Vector2 position;
+		Rectangle paddleRect;
+	public:
+		Paddle(Vector2 startPosition) {
+			position = startPosition;
+			paddleRect = { position.x, position.y, paddleSize.x, paddleSize.y };
+		}
+
+		/*
+		void Move(float speed) {
+
+		}*/
+
+		void Draw(){
+			DrawRectangle(position.x, position.y, paddleSize.x, paddleSize.y, WHITE);
+		}
+};
 
 int main() {
 
 	InitWindow(800, 600, "Pong");
+	//Vector2 playerPos = { 400, 300 };
+	Paddle player = Paddle({ 25 - paddleSize.x / 2, 300 - paddleSize.y / 2 });
+	Paddle enemy = Paddle({ 775 - paddleSize.x / 2, 300 - paddleSize.y / 2 });
 
 	while (!WindowShouldClose()) {
 
+
+		//Logic
+
+
+
+		//Drawing
 		BeginDrawing();
 		ClearBackground(BLACK);
+
+		player.Draw();
+		enemy.Draw();
+
 		EndDrawing();
 
 	}
